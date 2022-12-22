12307f5c9bfe753d7e220211e749e92368c970bb
Ryan Street <ryanstreet122@gmail.com>
2022-12-22 13:05:04 -0500

    User create & signup functional

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const tutorRoutes = require('./tutorRoutes')
+const userRoutes = require("./userRoutes")
 
 router.use('/tutors', tutorRoutes)
+router.use('/users', userRoutes)
 
 module.exports = router;
