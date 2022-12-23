b2ddc2e33f7233a1c81fb4a581013762e7f6388a
Jared Kim <jaredkim01@yahoo.com>
2022-12-22 21:02:36 -0600

    env file added

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-    "GetATutor_db",
-    "root",
-    "KwanghoMySqL-01",
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
         host: 'localhost',
         dialect: 'mysql',
